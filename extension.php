79e781d51b4d48158dfa661482209eeee0d6ca55
Mihails Simvulidi <mihails@simvulidi.lv>
2026-03-13 15:43:42 +0200

    Add CleanUrlExtension to remove unnecessary query parameters from URLs

---
+++ extension.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+final class CleanUrlExtension extends Minz_Extension
+{
+    public function init(): void
+    {
+        $this->registerHook('entry_before_insert', [self::class, 'cleanUrl']);
+    }
+
+    public static function cleanUrl(FreshRSS_Entry $entry): FreshRSS_Entry
+    {
+        $link = $entry->link(true);
+        $url = parse_url($link);
+        if (is_array($url) &&
+            isset($url['scheme']) &&
+            $url['scheme'] === 'https' &&
+            !isset($url['user']) &&
+            !isset($url['pass']) &&
+            isset($url['host']) &&
+            str_ends_with($url['host'], '.lsm.lv') &&
+            !isset($url['port']) &&
+            isset($url['path']))
+        {
+            $entry->_link($url['scheme'] . '://' . $url['host'] . $url['path']);
+        }
+        return $entry;
+    }
+}
